616aed5141eba12b376f77fa4dbbc2be0921e8a1
jurisaito <juri.saito@rakus-partners.co.jp>
2019-09-09 12:08:10 +0900

    first commit 新規にプロジェクトを作成

---
+++ src/main/java/com/example/Ec201907jApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec201907jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec201907jApplication.class, args);
+	}
+
+}
