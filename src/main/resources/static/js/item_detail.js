0bd7616331f903fb5da47cbf1a0f657af45c5fd0
jurisaito <juri.saito@rakus-partners.co.jp>
2019-09-24 09:07:08 +0900

    fix 商品詳細画面で小計金額が自動的に表示されるように修正

---
+++ src/main/resources/static/js/item_detail.js
@@ -0,0 +1,32 @@
+$(function() {
+	calc_price();
+	
+	$(".size").on("change", function(){
+		calc_price();
+	});
+	
+	$(".checkbox").on("change", function(){
+		calc_price();
+	});
+	
+	$("#quantity").on("change", function(){
+		calc_price();
+	});
+	
+	// 値段の計算をして変更する関数
+	function calc_price() {
+		var size = $(".size:checked").val();
+		var topping_count = $("#check input.checkbox:checked").length;
+		var piza_num = $('#quantity').val();
+		if(size == "M"){
+			var size_price = Number($('#priceM').text());
+			var topping_price = 200 * topping_count;
+		} else{
+			var size_price = Number($('#priceL').text());
+			var topping_price = 300 * topping_count;
+		}
+		var price = (size_price + topping_price) * piza_num;
+		$("#totalprice").text(price.toLocaleString());
+	}
+	
+});
