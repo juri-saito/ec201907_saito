b5708cf8581251420043674af4523da9db46637d
jurisaito <juri.saito@rakus-partners.co.jp>
2019-11-07 15:32:27 +0900

    fix SMTP4devを使用してメール送信するように変更

--- src/main/java/com/example/MailConfig.java
+++ src/main/java/com/example/MailConfig.java
@@ -14,18 +14,30 @@ public class MailConfig {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setHost("localhost");// ホスト名
+        mailSender.setPort(25);// ポート番号
 
-        mailSender.setUsername(System.getenv("GoogleMail"));
-        mailSender.setPassword(System.getenv("GooglePass"));
+        mailSender.setUsername("test@test.co.jp");
+        mailSender.setPassword("abc");
 
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.debug", "true");
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");// ユーザー認証
+//        props.put("mail.smtp.starttls.enable", "true");// 暗号化(TLS保護接続への接続)
+        props.put("mail.debug", "true");// デバッグ情報出力有無
 
         return mailSender;
     }
 }
+
+
+
+//mailSender.setHost("smtp.gmail.com");
+//mailSender.setPort(587);
+//mailSender.setPassword(System.getenv("GoogleMail"));
+//mailSender.setPassword(System.getenv("GooglePass"));
+
+//SMTP4dev使用・下記Server設定にチェックを入れる
+//8BITMIME(Don't corrupt non ASCII data)
+//STARTTLS(Explicit SSL/TLS)
+//AUTH(Allow authentication -any credentials accepted)
